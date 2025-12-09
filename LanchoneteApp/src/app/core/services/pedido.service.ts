b7b40fc0d5aff752e966c37546eaa8ad8fa9d8a7
felipef210 <rfelipe321@live.com>
2025-12-09 15:23:53 -0300

    feat: Notificação no header de pedido em aberto

--- LanchoneteApp/src/app/core/services/pedido.service.ts
+++ LanchoneteApp/src/app/core/services/pedido.service.ts
@@ -2,7 +2,7 @@ import { StatusPedidoEnum } from './../../shared/enums/StatusPedidoEnum';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, tap } from 'rxjs';
 import { PedidoDto } from '../../shared/models/pedido.models';
 
 @Injectable({
@@ -11,6 +11,9 @@ import { PedidoDto } from '../../shared/models/pedido.models';
 export class PedidoService {
   private readonly http: HttpClient = inject(HttpClient);
   private readonly url: string = environment.apiUrl + '/Pedido';
+
+  private temPedidosEmAbertoSubject = new BehaviorSubject<boolean>(false);
+  temPedidosEmAberto$ = this.temPedidosEmAbertoSubject.asObservable();
 
   getPedidos(): Observable<PedidoDto[]> {
     return this.http.get<PedidoDto[]>(`${this.url}/listar`);
@@ -24,15 +27,27 @@ export class PedidoService {
     return this.http.get<PedidoDto[]>(`${this.url}/pedidosEmAberto`);
   }
 
+  atualizarPedidosEmAberto() {
+    this.getPedidosEmAberto().subscribe(pedidos => {
+      this.temPedidosEmAbertoSubject.next(pedidos.length > 0);
+    });
+  }
+
   criarPedido(body: { itens: { produtoId: string; quantidade: number }[] }): Observable<PedidoDto> {
-    return this.http.post<PedidoDto>(`${this.url}`, body);
+    return this.http.post<PedidoDto>(`${this.url}`, body).pipe(
+      tap(() => this.atualizarPedidosEmAberto())
+    );
   }
 
   editarPedido(id: string, status: StatusPedidoEnum) {
-    return this.http.patch(`${this.url}/${id}`, { status });
+    return this.http.patch(`${this.url}/${id}`, { status }).pipe(
+      tap(() => this.atualizarPedidosEmAberto())
+    );
   }
 
   deletarPedido(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.url}/${id}`);
+    return this.http.delete<void>(`${this.url}/${id}`).pipe(
+      tap(() => this.atualizarPedidosEmAberto())
+    );
   }
 }
--- LanchoneteApp/src/app/shared/components/header/header.component.ts
+++ LanchoneteApp/src/app/shared/components/header/header.component.ts
@@ -2,23 +2,29 @@ import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { Router, RouterLink } from "@angular/router";
 import { AuthService } from '../../../core/services/auth.service';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { PedidoService } from '../../../core/services/pedido.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
   public readonly authService: AuthService = inject(AuthService);
+  public readonly pedidoservice: PedidoService = inject(PedidoService);
   private readonly carrinhoService: CarrinhoService = inject(CarrinhoService);
   private readonly router: Router = inject(Router);
 
   menuAberto: boolean = false;
   itens: number = 0;
 
+  existePedidosEmAberto$ = this.pedidoservice.temPedidosEmAberto$;
+
   ngOnInit() {
     this.carrinhoService.quantidadeItens$.subscribe((qtd) => this.itens = qtd);
+    this.pedidoservice.atualizarPedidosEmAberto();
   }
 
   logout() {
