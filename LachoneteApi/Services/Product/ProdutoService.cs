6d686b7d1795cd2fa2a458ef08cf7e10e3200cc6
felipef210 <rfelipe321@live.com>
2025-12-10 10:51:47 -0300

    fix: Ajuste no retorno do filtro de produtos por categoria

--- LachoneteApi/Services/Product/ProdutoService.cs
+++ LachoneteApi/Services/Product/ProdutoService.cs
@@ -47,7 +47,7 @@ public class ProdutoService : IProdutoService
     public async Task<List<ProdutoDto>> FiltrarPorCategoria(int categoria)
     {
         if (categoria <= 0 || categoria >= 5)
-            return _mapper.Map<List<ProdutoDto>>(_produtoRepository.ListarProdutos());
+            return _mapper.Map<List<ProdutoDto>>(await _produtoRepository.ListarProdutos());
 
         var query = _produtoRepository.FiltrarPorCategoria(categoria);
 
